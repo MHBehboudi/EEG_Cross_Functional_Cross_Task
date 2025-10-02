073221eb9eb32e4205b5ceeaf755a0c099474d49
Mohammad Hossein Behboudi <117489286+MHBehboudi@users.noreply.github.com>
2025-10-02 11:48:08 -0500

    Update train_ccd_slurm.sh

--- scripts/train_ccd_slurm.sh
+++ scripts/train_ccd_slurm.sh
@@ -33,4 +33,5 @@ mkdir -p data output
 python cli.py \
   --mini --epochs 1 --batch_size 128 --num_workers 4 \
   --data_dir ./data --out_dir ./output --save_zip \
-  --use_clusters --n_clusters 50 --projector_init kmeans
+  --use_clusters --n_clusters 50 --projector_init kmeans \
+  --amp auto --tf32 --compile
