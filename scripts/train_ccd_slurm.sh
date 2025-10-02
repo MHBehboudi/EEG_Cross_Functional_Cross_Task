09cbeca611beb1ffdcc837978e2bda745a239077
Mohammad Hossein Behboudi <117489286+MHBehboudi@users.noreply.github.com>
2025-10-02 13:40:01 -0500

    Update train_ccd_slurm.sh

--- scripts/train_ccd_slurm.sh
+++ scripts/train_ccd_slurm.sh
@@ -31,7 +31,8 @@ mkdir -p data output
 
 # Mini run (fast, startkit-like). For full training, remove --mini and bump --epochs.
 python cli.py \
-  --mini --epochs 1 --batch_size 128 --num_workers 4 \
+  --mini --epochs 10 --batch_size 128 --num_workers 4 \
   --data_dir ./data --out_dir ./output --save_zip \
   --use_ssl --clusters 20 --pcs_per_cluster 3 \
-  --ssl_epochs 1 --ssl_steps 120 --ssl_batch 128 --ssl_temp 0.2 --ssl_samples_per_ch 128
+  --ssl_epochs 5 --ssl_steps 120 --ssl_batch 128 --ssl_temp 0.2 \
+  --ssl_samples_per_ch 128
