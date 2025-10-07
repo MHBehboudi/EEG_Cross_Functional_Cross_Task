1227c69f9021f0dc4721bc1dbad43f8dc147f73b
Mohammad Hossein Behboudi <117489286+MHBehboudi@users.noreply.github.com>
2025-10-07 02:30:29 -0500

    Update contrastiveOverChannel.py

--- src/eegcfct/ssl/contrastiveOverChannel.py
+++ src/eegcfct/ssl/contrastiveOverChannel.py
@@ -23,3 +23,33 @@ class TinyChEncoder(nn.Module):
     h = h.mean(-1) #-> (B,C,T) -> (B,C)
     h = self.lin(h) # -> (B,C) -> (B,D)
     return h.unsqueze(1).repeat(1,x.shape[1],1)
+
+class TinyChLSTMEncoder(nn.Module):
+  def __init__(self, in_ch: int, emb_dim: int = 32):
+    super().__init__()
+    self.conv = nn.Sequential(
+                nn.Conv1d(in_ch, in_ch, padding = 2, kernel_size=5 , groups = in_ch),
+                nn.BatchNorm1d(in_ch),
+                nn.Dropout(0.2),
+                nn.GELU(),
+                nn.Conv1d(in_ch, in_ch, padding = 2, kernel_size=5 , groups = in_ch),
+                nn.BatchNorm1d(in_ch),
+                nn.Dropout(0.2),
+                nn.GELU(),
+    ) #(B,C,T))
+    self.lstm = nn.LSTM(input_size = 1, hidden_size = 16, bidirectional = True)# (C*2*H, T)
+    self.lin = nn.Linear(2*16, emb_dim)
+    def forward(self,x: torch.Tensor) -> torch.Tensor:
+      #input is a time_window (B,C,T)
+      B , C, T = x.shape
+      h = self.conv(x) #(B,C,T) -> (B,C,T)
+      h = h.reshape(B*C,T,1).transpose(0,1) #(B,C,T) -> (B*C ,T, 1)
+      _, (h_n, _) = self.lstm(h) # (B*C ,T, 1) -> (2, B*C, H) 
+      h = h_n.transpose(0,1).reshape(B*C,-1) # (2, B*C, H) -> (B*C, 2* H)
+      h = self.lin(h) # (B*C, 2 * H)   -> (B*C,D)
+
+      return h.reshape(B, C,emb_dim)
+
+
+      
+                
